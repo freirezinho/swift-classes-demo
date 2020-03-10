781be0e9d01cd2f2b9535d226dd00eadd984d4bd
Saulo Freire <gbsaulo@gmail.com>
2020-03-10 08:56:34 -0300

    Initial Commit

---
+++ classesDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  classesDemo
+//
+//  Created by Saulo Freire on 10/03/20.
+//  Copyright © 2020 Freire. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
